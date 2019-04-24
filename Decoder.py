469c3a4ecbdb2494836e2751100af2724133fe6b
Aulia Ahmad Nabil <39146007+BrondoL@users.noreply.github.com>
2019-04-24 18:49:51 +0700

    Just Run This Script Dude :))

--- Decoder.py
+++ Decoder.py
@@ -5,7 +5,7 @@ passwd = base64.b64decode("c2VtdWFpbmlkYXJpYWxsYWg=")
 if(user == passwd):	
 	f = open("Solver.cpp", "r")
 	plain = base64.b64decode(f.read())
-	f = open("hasil.cpp", "w")
+	f = open("loveyou.cpp", "w")
 	f.write(plain)
 	f.close()
 	print("Berhasil")
